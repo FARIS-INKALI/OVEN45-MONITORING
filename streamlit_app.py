0729ac1f691571eb17c5d57537c8896809d82eae
FARIS-INKALI <130633334+FARIS-INKALI@users.noreply.github.com>
2023-05-03 08:41:17 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,7 +40,7 @@ df = get_data(worksheet)
 def highlight_outdated(s):
     return ['background-color: green']*len(s) if datetime.strptime(s['Tanggal Keluar'], "%Y-%m-%d").date() > date.today() else ['background-color: red']*len(s)
 
-st.dataframe(df[1:].style.apply(highlight_survived, axis=1), use_container_width=True)
+st.dataframe(df[1:].style.apply(highlight_outdated, axis=1), use_container_width=True)
 # st.dataframe(df[1:], use_container_width=True)
 
 # tambah dataframe gabungan index dengan nama sample
